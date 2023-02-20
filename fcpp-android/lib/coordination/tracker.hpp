2ac3d1e1a7b997a2c2d1e0e1e22a23cc857f51ea
Giorgio Audrito <giorgio.audrito@gmail.com>
2023-02-20 19:06:27 +0100

    added "not_alone" checking whether I have ever seen anybody else

--- fcpp-android/lib/coordination/tracker.hpp
+++ fcpp-android/lib/coordination/tracker.hpp
@@ -12,6 +12,7 @@
 #include "lib/coordination.hpp"
 #include "lib/common/plot.hpp"
 #include "lib/option/aggregator.hpp"
+#include "lib/coordination/past_ctl.hpp"
 
 
 /**
@@ -24,6 +25,8 @@ namespace coordination {
 
 //! @brief Tags used in the node storage.
 namespace tags {
+    //! @brief Whether I have ever seen somebody else.
+    struct not_alone {};
     //! @brief Time in seconds between transmission rounds.
     struct round_period {};
     //! @brief The retain time for messages.
@@ -45,6 +48,7 @@ FUN void tracker(ARGS) { CODE
     using namespace tags;
     node.frequency(1.0/node.storage(round_period{}));
     node.message_threshold(node.storage(retain_time{}));
+    node.storage(not_alone{}) = logic::P(CALL, node.size() > 1);
     node.storage(nbr_lags{}) = node.nbr_lag();
     node.storage(round_count{}) = counter(CALL, uint16_t{1});
     node.storage(global_clock{}) = shared_clock(CALL);
@@ -52,16 +56,20 @@ FUN void tracker(ARGS) { CODE
     node.storage(max_msg{}) = gossip_max(CALL, (uint16_t)node.storage(cur_msg{}));
 }
 //! @brief Export list for tracker.
-FUN_EXPORT tracker_t = export_list<counter_t<uint16_t>, shared_clock_t, gossip_max_t<uint16_t>>;
+FUN_EXPORT tracker_t = export_list<
+    past_ctl_t,
+    counter_t<uint16_t>, shared_clock_t, gossip_max_t<uint16_t>
+>;
 //! @brief Storage list for tracker.
 FUN_EXPORT tracker_s = storage_list<
-    tags::round_period,       times_t,
-    tags::retain_time,        times_t,
-    tags::nbr_lags,           field<times_t>,
-    tags::round_count,        uint16_t,
-    tags::global_clock,       times_t,
-    tags::cur_msg,            uint8_t,
-    tags::max_msg,            uint8_t
+    tags::not_alone,        bool,
+    tags::round_period,     times_t,
+    tags::retain_time,      times_t,
+    tags::nbr_lags,         field<times_t>,
+    tags::round_count,      uint16_t,
+    tags::global_clock,     times_t,
+    tags::cur_msg,          uint8_t,
+    tags::max_msg,          uint8_t
 >;
 //! @brief Aggregator list for tracker.
 FUN_EXPORT tracker_a = storage_list<
